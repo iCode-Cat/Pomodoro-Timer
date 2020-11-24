1abbb8e807fcf7c4dc1d165e057587656faeb573
iCode-Cat <devranboyac8@gmail.com>
2020-11-24 16:43:19 +0200

    main

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const reset = document.querySelector('.reset');
 const pomodoro25 = document.querySelector('.p25');
 const breakShort = document.querySelector('.p5');
 const breakLong = document.querySelector('.p15');
+const ring = document.querySelector('.ring');
+const title = document.querySelector('title');
 //Default Options
 let minute = 25;
 let second = 00;
 fStart()
+
 //Start and Pause the function.  
 let go = false;
 let stop = false;
 let breakS = false;
+let play = true;
 //Reset the timer or set it to 25 minutes again. 
 this.addEventListener('click' , (event)=> {
     if (event.target === pomodoro25 || event.target === reset ) {
@@ -42,6 +46,7 @@ function breaks(mi){
 //When user click start, go turns true, functions start or continue.
 start.addEventListener('click', () => {
     go = true;
+    play = true;
 })
 //Pause stops function. It can continue by clicking start button.
 pause.addEventListener('click', () => {
@@ -53,6 +58,10 @@ function fStart() {
     setInterval(() => {
         if(minute == 0 && second == 00) {
             go = false;
+            if(play) {
+                playRing()
+            }
+            
         }
     }, 1000);
     //Check every one second. 
@@ -65,12 +74,16 @@ function fStart() {
                 second = 60
                 minute--
             }
-
-            
-
             second--
             clock.innerText = `${minute}:${second}`
+            title.innerText = `${minute}:${second}`
+    
         }
     }, 1000);
 }
 
+function playRing() {
+    ring.load();
+    ring.play();
+    play = false;
+}
